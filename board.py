f8a5195ee1342abf44285affd6d185fc2989fd01
t-kamagata <t-kamagata@needsshare.co.jp>
2023-04-26 15:17:20 +0900

    yeah p

--- board.py
+++ board.py
@@ -18,10 +18,12 @@ class Board:
         self.board[3][4] = WHITE
         self.board[4][3] = WHITE
         self.board[4][4] = BLACK
+        self.update_reversable()
     
     def update_reversable(self):
         for i in range(1, 7):
             for j in range(1, 7):
+                count_sum = 0
                 for dir in range(8):
                     (x, y) = self.indexToVec2(dir)
                     if self.turn == WHITE:
@@ -30,12 +32,70 @@ class Board:
                             while True:
                                 if self.board[i + (x*count)][j + (y*count)] == EMPTY or\
                                 self.board[i + (x*count)][j + (y*count)] == WALL:
-                                    self.cnt_reversable[i][j] = 0
+                                    self.cnt_reversable[i][j] = count_sum
                                     break
+                                elif self.board[i + (x*count)][j + (y*count)] == WHITE:
+                                    count_sum += count-1
+                                    self.cnt_reversable[i][j] = count_sum
+                                    break
+                                else:
+                                    count += 1
                         else:
-                            self.cnt_reversable[i][j] = 0
+                            self.cnt_reversable[i][j] = count_sum
                     else:
-                        pass
+                        if self.board[i+x][j+y] == WHITE:
+                            count = 1
+                            while True:
+                                if self.board[i + (x*count)][j + (y*count)] == EMPTY or\
+                                self.board[i + (x*count)][j + (y*count)] == WALL:
+                                    self.cnt_reversable[i][j] = count_sum
+                                    break
+                                elif self.board[i + (x*count)][j + (y*count)] == BLACK:
+                                    count_sum += count-1
+                                    self.cnt_reversable[i][j] = count_sum
+                                    break
+                                else:
+                                    count += 1
+                        else:
+                            self.cnt_reversable[i][j] = count_sum
+    
+    def puttable(self, x, y):
+        return self.cnt_reversable[x][y] > 0
+    
+    def put(self, i, j):
+        for dir in range(8):
+            (x, y) = self.indexToVec2(dir)
+            if self.turn == WHITE:
+                if self.board[i+x][j+y] == BLACK:
+                    count = 1
+                    while True:
+                        if self.board[i + (x*count)][j + (y*count)] == EMPTY or\
+                        self.board[i + (x*count)][j + (y*count)] == WALL:
+                            self.cnt_reversable[i][j] = 0
+                            break
+                        elif self.board[i + (x*count)][j + (y*count)] == WHITE:
+                            for t in range(1, count):
+                                self.board[i + (x*t)][j + (y*t)] = BLACK
+                            break
+                        else:
+                            count += 1
+                else:
+                    self.cnt_reversable[i][j] = 0
+            else:
+                if self.board[i+x][j+y] == BLACK:
+                    count = 1
+                    while True:
+                        if self.board[i + (x*count)][j + (y*count)] == EMPTY or\
+                        self.board[i + (x*count)][j + (y*count)] == WALL:
+                            self.cnt_reversable[i][j] = 0
+                            break
+                        elif self.board[i + (x*count)][j + (y*count)] == BLACK:
+                            self.cnt_reversable[i][j] = count
+                            break
+                        else:
+                            count += 1
+                else:
+                    self.cnt_reversable[i][j] = 0
     
     def flipTurn(self):
         if self.turn == BLACK:
@@ -72,5 +132,5 @@ class Board:
         for i in range(6):
             print(f"{i+1:^2}", end="")
             for j in range(6):
-                print(f"{self.board[i+1][j+1]:^2}", end="")
+                print(f"{'p' if self.puttable(i+1, j+1) else self.board[i+1][j+1]:^2}", end="")
             print("")
--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from board import Board
 
+def get2Input():
+    inputs = input("入力(行, 列)＞").strip().split()
+    length = len(inputs)
+    if length == 2:
+        return (True, (inputs[0], inputs[1]))
+    elif length > 2:
+        print("要素が多すぎます")
+        return (False, (0, 0))
+        pass
+    else:
+        print("要素が少ないです")
+        return (False, (0, 0))
+    
 b = Board()
 b.print()
+
+inputs = ()
+
+while True:
+    (success, inputs) = get2Input()
+    if success:
+        break
+b.put(int(inputs[0]), int(inputs[1]))
